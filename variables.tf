af87333a879c76c985de6f773110513f410a3c4d
Felddy <markf@geekpad.com>
2020-03-03 17:27:35 -0500

    Remove user_account variable. It is now looked up.

--- variables.tf
+++ variables.tf
@@ -4,16 +4,16 @@
 # You must provide a value for each of these parameters.
 # ------------------------------------------------------------------------------
 
-variable "aws_region" {
-  description = "The AWS region to deploy into (e.g. us-east-1)"
-  default     = "us-east-1"
-}
-
 # ------------------------------------------------------------------------------
 # OPTIONAL PARAMETERS
 #
 # These parameters have reasonable defaults.
 # ------------------------------------------------------------------------------
+
+variable "aws_region" {
+  description = "The AWS region to deploy into (e.g. us-east-1)"
+  default     = "us-east-1"
+}
 
 variable "tags" {
   type        = map(string)
@@ -29,8 +29,4 @@ variable "route53resourcechange_role_description" {
 variable "route53resourcechange_role_name" {
   description = "The name to assign the IAM role (as well as the corresponding policy) that allows sufficient permissions to modify resource records in the DNS zone."
   default     = "Route53ResourceChange-cyber.dhs.gov"
-}
-
-variable "users_account_id" {
-  description = "The ID of the users account.  This account will be allowed to assume the role that allows sufficient permissions to provision all AWS resources in the Images account."
 }

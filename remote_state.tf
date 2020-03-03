99eed1711347d68afdedca7c25170e0892628689
Felddy <markf@geekpad.com>
2020-03-03 16:30:06 -0500

    Fetch upstream state.

---
+++ remote_state.tf
@@ -0,0 +1,14 @@
+data "terraform_remote_state" "dns" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-accounts/dns.tfstate"
+  }
+
+  workspace = "production"
+}

2a1341f0e2f792925f8c5c4ae3eac0a92d561b08
Felddy <markf@geekpad.com>
2020-03-03 16:34:58 -0500

    Add default variables.

--- variables.tf
+++ variables.tf
@@ -6,14 +6,7 @@
 
 variable "aws_region" {
   description = "The AWS region to deploy into (e.g. us-east-1)"
-}
-
-variable "aws_availability_zone" {
-  description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)"
-}
-
-variable "subnet_id" {
-  description = "The ID of the AWS subnet to deploy into (e.g. subnet-0123456789abcdef0)"
+  default     = "us-east-1"
 }
 
 # ------------------------------------------------------------------------------
@@ -26,4 +19,18 @@ variable "tags" {
   type        = map(string)
   description = "Tags to apply to all AWS resources created"
   default     = {}
+}
+
+variable "route53resourcechange_role_description" {
+  description = "The description to associate with the IAM role (as well as the corresponding policy) that allows sufficient permissions to modify resource records in the DNS zone."
+  default     = "Allows sufficient permissions to modify resource records in the DNS zone."
+}
+
+variable "route53resourcechange_role_name" {
+  description = "The name to assign the IAM role (as well as the corresponding policy) that allows sufficient permissions to modify resource records in the DNS zone."
+  default     = "Route53ResourceChange-cyber.dhs.gov"
+}
+
+variable "users_account_id" {
+  description = "The ID of the users account.  This account will be allowed to assume the role that allows sufficient permissions to provision all AWS resources in the Images account."
 }

e8200cea7d244d4cee349c4b0efe59712c5a59ae
Felddy <markf@geekpad.com>
2020-03-03 16:42:31 -0500

    Add zone to the outputs.

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "cyber_dhs_gov_zone" {
+  value       = aws_route53_zone.cyber_dhs_gov
+  description = "The cyber.dhs.gov public hosted zone."
+}
+
 output "route53resourcechange_role" {
   value       = aws_iam_role.route53resourcechange_role
   description = "IAM role that allows sufficient permissions to modify resource records in the DNS zone."

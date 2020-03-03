45236e1460dcf2bdc0b9676ad7b33e570544ce47
Felddy <markf@geekpad.com>
2020-03-03 17:27:58 -0500

    Fetch master remote state.

--- remote_state.tf
+++ remote_state.tf
@@ -1,3 +1,18 @@
+data "terraform_remote_state" "master" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-accounts/master.tfstate"
+  }
+
+  workspace = "production"
+}
+
 data "terraform_remote_state" "dns" {
   backend = "s3"
 

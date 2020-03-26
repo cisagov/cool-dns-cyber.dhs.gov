4af3f7482f676dc732c903bce89791c1c7d7b21a
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-03-25 22:53:38 -0400

    Add a second permission that is also required to search for the desired hosted zone

--- route53resourcechange_policy.tf
+++ route53resourcechange_policy.tf
@@ -28,6 +28,7 @@ data "aws_iam_policy_document" "route53resourcechange_doc" {
   statement {
     actions = [
       "route53:ListHostedZones",
+      "route53:ListTagsForResource",
     ]
 
     resources = ["*"]

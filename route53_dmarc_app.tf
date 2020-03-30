6bf9b290dfff90f497fa4b2283aa3082ceab54e8
Felddy <markf@geekpad.com>
2020-03-30 16:36:46 -0400

    Automate domain verification for dmarc subdomian.

--- route53_dmarc_app.tf
+++ route53_dmarc_app.tf
@@ -2,10 +2,23 @@
 # Resource records that support the DMARC application.
 # ------------------------------------------------------------------------------
 
+# ------------------------------------------------------------------------------
+# Evaluate expressions for use throughout this file.
+# ------------------------------------------------------------------------------
+locals {
+  dmarc_domain_name = "dmarc.${aws_route53_zone.cyber_dhs_gov.name}"
+}
+
+resource "aws_ses_domain_identity" "dmarc_identity" {
+  provider = aws.route53resourcechange
+
+  domain = local.dmarc_domain_name
+}
+
 resource "aws_route53_record" "dmarc_MX" {
   provider = aws.route53resourcechange
 
-  name    = "dmarc.${aws_route53_zone.cyber_dhs_gov.name}"
+  name    = local.dmarc_domain_name
   records = ["10 inbound-smtp.us-east-1.amazonaws.com"]
   ttl     = 1800
   type    = "MX"
@@ -15,8 +28,8 @@ resource "aws_route53_record" "dmarc_MX" {
 resource "aws_route53_record" "_amazonses_dmarc_TXT" {
   provider = aws.route53resourcechange
 
-  name    = "_amazonses.dmarc.${aws_route53_zone.cyber_dhs_gov.name}"
-  records = ["OHUkpPtvpdayRxxI15t6tGA38DHwt+JnJ1jpaAE4Sa8="]
+  name    = "_amazonses.${local.dmarc_domain_name}"
+  records = ["${aws_ses_domain_identity.dmarc_identity.verification_token}"]
   ttl     = 60
   type    = "TXT"
   zone_id = aws_route53_zone.cyber_dhs_gov.zone_id
@@ -25,7 +38,7 @@ resource "aws_route53_record" "_amazonses_dmarc_TXT" {
 resource "aws_route53_record" "wildcard_report_dmarc_TXT" {
   provider = aws.route53resourcechange
 
-  name    = "*._report._dmarc.dmarc.${aws_route53_zone.cyber_dhs_gov.name}"
+  name    = "*._report._dmarc.${local.dmarc_domain_name}"
   records = ["v=DMARC1"]
   ttl     = 300
   type    = "TXT"
@@ -35,7 +48,7 @@ resource "aws_route53_record" "wildcard_report_dmarc_TXT" {
 resource "aws_route53_record" "dkim1_dmarc_CNAME" {
   provider = aws.route53resourcechange
 
-  name    = "6na6lcj7onl5bco4ytfj4ud7p6t7kvtp._domainkey.dmarc.${aws_route53_zone.cyber_dhs_gov.name}"
+  name    = "6na6lcj7onl5bco4ytfj4ud7p6t7kvtp._domainkey.${local.dmarc_domain_name}"
   records = ["6na6lcj7onl5bco4ytfj4ud7p6t7kvtp.dkim.amazonses.com"]
   ttl     = 1800
   type    = "CNAME"
@@ -45,7 +58,7 @@ resource "aws_route53_record" "dkim1_dmarc_CNAME" {
 resource "aws_route53_record" "dkim2_dmarc_CNAME" {
   provider = aws.route53resourcechange
 
-  name    = "nsbndtrubsyckjqnb4wkv6xdkrqe3dk5._domainkey.dmarc.${aws_route53_zone.cyber_dhs_gov.name}"
+  name    = "nsbndtrubsyckjqnb4wkv6xdkrqe3dk5._domainkey.${local.dmarc_domain_name}"
   records = ["nsbndtrubsyckjqnb4wkv6xdkrqe3dk5.dkim.amazonses.com"]
   ttl     = 1800
   type    = "CNAME"
@@ -55,7 +68,7 @@ resource "aws_route53_record" "dkim2_dmarc_CNAME" {
 resource "aws_route53_record" "dkim3_dmarc_CNAME" {
   provider = aws.route53resourcechange
 
-  name    = "yhfkaco3ukhtbowt2bdvfz5czwuofitm._domainkey.dmarc.${aws_route53_zone.cyber_dhs_gov.name}"
+  name    = "yhfkaco3ukhtbowt2bdvfz5czwuofitm._domainkey.${local.dmarc_domain_name}"
   records = ["yhfkaco3ukhtbowt2bdvfz5czwuofitm.dkim.amazonses.com"]
   ttl     = 1800
   type    = "CNAME"

34e5cb40a3b04d3f5d2164d1dc287a5f53eda028
Felddy <markf@geekpad.com>
2020-03-30 16:36:07 -0400

    Add permissions required to verify a domain in SES. Note that if/when terraform tries to delete a verification AWS is getting upset about the trailing period on the name.  I had to delete the verification by hand.

--- route53resourcechange_policy.tf
+++ route53resourcechange_policy.tf
@@ -21,6 +21,16 @@ data "aws_iam_policy_document" "route53resourcechange_doc" {
 
     resources = ["arn:aws:route53:::change/*"]
   }
+
+  statement {
+    actions = [
+      "ses:DeleteIdentity",
+      "ses:GetIdentityVerificationAttributes",
+      "ses:VerifyDomainIdentity",
+    ]
+
+    resources = ["*"]
+  }
 }
 
 resource "aws_iam_policy" "route53resourcechange_policy" {

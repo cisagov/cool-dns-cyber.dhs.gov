be89cdc6abf4912a946f3d5be14da3eca232a3d6
Dave Redmin <david.redmin@trio.dhs.gov>
2020-12-10 11:33:09 -0500

    Use remote state output to set NS record names

--- route53_pca.tf
+++ route53_pca.tf
@@ -10,7 +10,7 @@
 resource "aws_route53_record" "pca_production_NS" {
   provider = aws.route53resourcechange
 
-  name    = "con-pca.cool.${aws_route53_zone.cyber_dhs_gov.name}"
+  name    = data.terraform_remote_state.pca_production.outputs.hosted_zone_name
   ttl     = 86400
   type    = "NS"
   records = data.terraform_remote_state.pca_production.outputs.hosted_zone_name_servers
@@ -24,7 +24,7 @@ resource "aws_route53_record" "pca_production_NS" {
 resource "aws_route53_record" "pca_staging_NS" {
   provider = aws.route53resourcechange
 
-  name    = "con-pca.staging.cool.${aws_route53_zone.cyber_dhs_gov.name}"
+  name    = data.terraform_remote_state.pca_staging.outputs.hosted_zone_name
   ttl     = 30
   type    = "NS"
   records = data.terraform_remote_state.pca_staging.outputs.hosted_zone_name_servers

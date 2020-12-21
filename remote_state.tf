380ad0b51456282dc3a599df9bf9e9bc56f22f37
dav3r <daver@geekpad.com>
2020-12-21 17:14:14 -0500

    Revert "Revert "Add NS records for Con-PCA""

--- remote_state.tf
+++ remote_state.tf
@@ -3,6 +3,21 @@
 # outputs of one or more Terraform configurations as input data for this configuration.
 # ------------------------------------------------------------------------------
 
+data "terraform_remote_state" "dns" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-accounts/dns.tfstate"
+  }
+
+  workspace = "production"
+}
+
 data "terraform_remote_state" "master" {
   backend = "s3"
 
@@ -18,7 +33,7 @@ data "terraform_remote_state" "master" {
   workspace = "production"
 }
 
-data "terraform_remote_state" "dns" {
+data "terraform_remote_state" "pca_production" {
   backend = "s3"
 
   config = {
@@ -27,8 +42,23 @@ data "terraform_remote_state" "dns" {
     dynamodb_table = "terraform-state-lock"
     profile        = "cool-terraform-backend"
     region         = "us-east-1"
-    key            = "cool-accounts/dns.tfstate"
+    key            = "con-pca-cicd/terraform.tfstate"
   }
 
   workspace = "production"
+}
+
+data "terraform_remote_state" "pca_staging" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "con-pca-cicd/terraform.tfstate"
+  }
+
+  workspace = "staging"
 }

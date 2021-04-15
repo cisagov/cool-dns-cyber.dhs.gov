940df810c96b0df69dd73653db73aaeb2073254a
David Redmin <david.redmin@trio.dhs.gov>
2021-04-15 16:35:45 -0400

    Trust Domain Manager accounts to use this role
    Since the list of accounts has grown, I also took this opportunity to 
    sort the accounts alphabetically and clean up the comments a bit.

--- sessendemail_role.tf
+++ sessendemail_role.tf
@@ -12,11 +12,19 @@ data "aws_iam_policy_document" "sessendemail_assume_role_doc" {
 
     principals {
       type = "AWS"
-      # The users account needs to send an alert email when a new user
-      # is created.  The INL and PCA accounts need to send email for the PCA
-      # work they are doing.  The CyHy account needs to email the CyHy
-      # and BOD 18-01 reports and the CybEx scorecard.
-      identifiers = concat([local.users_account_id], local.inl_account_ids, local.pca_account_ids, [var.cyhy_account_id])
+      # Account usage needs:
+      # - CyHy: Email the CyHy and BOD 18-01 reports and the CybEx scorecard
+      # - Domain Manager: Send various notification emails to internal users
+      # - INL: Test sending phishing emails for Phishing Campaign Assessments
+      # - PCA: Send phishing emails for Phishing Campaign Assessments
+      # - Users: Send an alert email when a new user account is created
+      identifiers = concat(
+        [var.cyhy_account_id],
+        local.domainmanager_account_ids,
+        local.inl_account_ids,
+        local.pca_account_ids,
+        [local.users_account_id]
+      )
     }
   }
 }

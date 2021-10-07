3180ddc25f1ecd0e7d88c063aa2097865fad8d36
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-10-06 21:53:40 -0400

    Add a role allowing read-only access to this root module's Terraform state
    Adding such a role also necessitates adding remote state and a
    provider for the Terraform account, as well as two new input
    variables.

--- remote_state.tf
+++ remote_state.tf
@@ -62,3 +62,20 @@ data "terraform_remote_state" "pca_staging" {
 
   workspace = "staging"
 }
+
+data "terraform_remote_state" "terraform" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-accounts/terraform.tfstate"
+  }
+
+  # There is only one environment for this account, so there is
+  # no need to match the current Terraform workspace.
+  workspace = "production"
+}
--- variables.tf
+++ variables.tf
@@ -15,6 +15,12 @@ variable "cyhy_account_id" {
 # These parameters have reasonable defaults.
 # ------------------------------------------------------------------------------
 
+variable "additional_remote_state_account_ids" {
+  type        = list(string)
+  description = "A list of accound IDs corresponding to additional accounts that should have permission to assume the role to read this root module's remote state (e.g. [\"123456789012\"])."
+  default     = []
+}
+
 variable "aws_region" {
   type        = string
   description = "The AWS region to communicate with."
@@ -25,6 +31,12 @@ variable "cloudfront_zone_id" {
   type        = string
   description = "The ID of the Cloudfront hosted zone. This is set by AWS and is a constant across all Cloudfront distributions."
   default     = "Z2FDTNDATAQYW2"
+}
+
+variable "read_terraform_state_role_name" {
+  type        = string
+  description = "The name to assign the IAM role and policy that allows read-only access to the cool-dns.cyber.dhs.gov state in the S3 bucket where Terraform state is stored."
+  default     = "ReadCyberDhsGovTerraformState"
 }
 
 variable "route53resourcechange_role_description" {

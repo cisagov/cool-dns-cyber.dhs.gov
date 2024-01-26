393e6794e2749f6055d44acba31ae816728f9789
Jeremy Morrow <jeremy.morrow@cisa.dhs.gov>
2024-01-26 10:57:16 -0500

    Replace SADR record with RSAA record

--- route53_rsaa_app.tf
+++ route53_rsaa_app.tf
@@ -1,14 +1,15 @@
 # ------------------------------------------------------------------------------
-# Resource records that support SADR
+# Resource records that support RSAA (Repository for Software Attestations and
+# Artifacts)
 
 # ------------------------------------------------------------------------------
 # Staging entries
 # ------------------------------------------------------------------------------
 
-resource "aws_route53_record" "sadr_stage_CNAME" {
+resource "aws_route53_record" "rsaa_stage_CNAME" {
   provider = aws.route53resourcechange
-  name     = "staging.sadr.${aws_route53_zone.cyber_dhs_gov.name}"
-  records  = ["sadr-stage-1940583289.us-gov-west-1.elb.amazonaws.com"]
+  name     = "staging.rsaa.${aws_route53_zone.cyber_dhs_gov.name}"
+  records  = ["rsaa-stage-955379685.us-gov-west-1.elb.amazonaws.com"]
   ttl      = 300
   type     = "CNAME"
   zone_id  = aws_route53_zone.cyber_dhs_gov.zone_id

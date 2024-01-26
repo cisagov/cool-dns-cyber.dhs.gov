102c8b1bf16888fc5b6a61ac5a034aa216572d77
Jeremy Morrow <jeremy.morrow@cisa.dhs.gov>
2024-01-26 11:00:41 -0500

    Add CNAME record for development environment

--- route53_rsaa_app.tf
+++ route53_rsaa_app.tf
@@ -3,6 +3,18 @@
 # Artifacts)
 
 # ------------------------------------------------------------------------------
+# Development entries
+# ------------------------------------------------------------------------------
+
+resource "aws_route53_record" "rsaa_dev_CNAME" {
+  provider = aws.route53resourcechange
+  name     = "dev.rsaa.${aws_route53_zone.cyber_dhs_gov.name}"
+  records  = ["rsaa-dev-674180318.us-gov-west-1.elb.amazonaws.com"]
+  ttl      = 300
+  type     = "CNAME"
+  zone_id  = aws_route53_zone.cyber_dhs_gov.zone_id
+}
+    # ------------------------------------------------------------------------------
 # Staging entries
 # ------------------------------------------------------------------------------
 

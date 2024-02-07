f7b7bd788ddf96bcfb7c4954407ff4c105a1440f
Jeremy Morrow <jeremy.morrow@cisa.dhs.gov>
2024-02-07 14:19:00 -0500

    Add entries to support PIV for RSAA

--- route53_rsaa_app.tf
+++ route53_rsaa_app.tf
@@ -15,6 +15,16 @@ resource "aws_route53_record" "rsaa_dev_CNAME" {
   zone_id  = aws_route53_zone.cyber_dhs_gov.zone_id
 }
 
+resource "aws_route53_record" "rsaa_dev_piv_CNAME" {
+  provider = aws.route53resourcechange
+  name     = "piv.dev.rsaa.${aws_route53_zone.cyber_dhs_gov.name}"
+  records  = ["rsaa-dev-piv-651272849.us-gov-west-1.elb.amazonaws.com"]
+  ttl      = 300
+  type     = "CNAME"
+  zone_id  = aws_route53_zone.cyber_dhs_gov.zone_id
+}
+
+
 # ------------------------------------------------------------------------------
 # Staging entries
 # ------------------------------------------------------------------------------
@@ -23,6 +33,15 @@ resource "aws_route53_record" "rsaa_stage_CNAME" {
   provider = aws.route53resourcechange
   name     = "staging.rsaa.${aws_route53_zone.cyber_dhs_gov.name}"
   records  = ["rsaa-stage-955379685.us-gov-west-1.elb.amazonaws.com"]
+  ttl      = 300
+  type     = "CNAME"
+  zone_id  = aws_route53_zone.cyber_dhs_gov.zone_id
+}
+
+resource "aws_route53_record" "rsaa_stage_piv_CNAME" {
+  provider = aws.route53resourcechange
+  name     = "piv.staging.rsaa.${aws_route53_zone.cyber_dhs_gov.name}"
+  records  = ["rsaa-stage-piv-144377488.us-gov-west-1.elb.amazonaws.com"]
   ttl      = 300
   type     = "CNAME"
   zone_id  = aws_route53_zone.cyber_dhs_gov.zone_id

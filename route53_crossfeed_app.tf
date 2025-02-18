eeabb4c916a2528c0b62f8f15841b50563bcf4bc
cduhn17 <craig.duhn@nttdatafed.com>
2025-02-18 07:34:20 -0600

    Update TXT acme value for api.staging-cd

--- route53_crossfeed_app.tf
+++ route53_crossfeed_app.tf
@@ -454,7 +454,7 @@ resource "aws_route53_record" "crossfeed_api_staging_cd_TXT" {
 
   name = "_acme-challenge.api.staging-cd.crossfeed.${aws_route53_zone.cyber_dhs_gov.name}"
   records = [
-    "b46k-0MntwazFRg_hz4f1zOHuboertdcrb0CSd-u9kQ",
+    "cT0KQCAL1VHuTAXx-SBc_5UN-Eml1CNMTRk2VuJSKHk",
   ]
   ttl     = 3000
   type    = "TXT"

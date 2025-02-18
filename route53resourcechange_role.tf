e9cc6f68243d1a4aa7dab7175ea0eb167149aca3
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2025-02-18 16:12:16 -0500

    Allow the role that can create and modify records in the cyber.dhs.gov zone to be assumed by the accounts specified in the new additional_route53_resource_change_account_ids variable

--- route53resourcechange_role.tf
+++ route53resourcechange_role.tf
@@ -3,10 +3,33 @@
 # resource records in the DNS zone.
 # ------------------------------------------------------------------------------
 
+# An IAM policy document that allows the Users account and any
+# additionally-specified accounts to assume the role.
+data "aws_iam_policy_document" "route53resourcechange_assume_role_doc" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession",
+    ]
+
+    principals {
+      # Account usage needs:
+      # - Additional: The use case for each account should be documented with
+      #   a comment in the Terraform variables file
+      # - Users: Ongoing maintenance of the cyber.dhs.gov zone
+      identifiers = concat(
+        var.additional_route53_resource_change_account_ids,
+        [local.users_account_id]
+      )
+      type = "AWS"
+    }
+  }
+}
+
 resource "aws_iam_role" "route53resourcechange_role" {
   provider = aws.dnsprovisionaccount
 
-  assume_role_policy = data.aws_iam_policy_document.assume_role_doc.json
+  assume_role_policy = data.aws_iam_policy_document.route53resourcechange_assume_role_doc.json
   description        = var.route53resourcechange_role_description
   name               = var.route53resourcechange_role_name
 }

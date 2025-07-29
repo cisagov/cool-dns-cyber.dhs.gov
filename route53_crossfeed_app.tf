7d1b042f35aed63fb9a54de2b159164a68ab07ab
cduhn17 <87447403+cduhn17@users.noreply.github.com>
2025-07-29 15:34:25 -0500

    Update route53_crossfeed_app.tf
    Co-authored-by: dav3r <daver@geekpad.com>

--- route53_crossfeed_app.tf
+++ route53_crossfeed_app.tf
@@ -11,7 +11,7 @@ resource "aws_route53_record" "crossfeed_prod_CNAME" {
   provider = aws.route53resourcechange
 
   name    = "crossfeed.${aws_route53_zone.cyber_dhs_gov.name}"
-  records = ["cyber-hygiene.cisa.dhs.gov.", ]
+  records = ["cyber-hygiene.cisa.dhs.gov", ]
   ttl     = 300
   type    = "CNAME"
   zone_id = aws_route53_zone.cyber_dhs_gov.zone_id

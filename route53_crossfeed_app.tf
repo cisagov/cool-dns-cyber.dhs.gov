781ee568fcc5b8812570e1f98724368bc8732b1a
cduhn17 <craig.duhn@nttdatafed.com>
2025-07-29 15:04:29 -0500

    Update order of variables at CNAME record

--- route53_crossfeed_app.tf
+++ route53_crossfeed_app.tf
@@ -11,13 +11,10 @@ resource "aws_route53_record" "crossfeed_prod_CNAME" {
   provider = aws.route53resourcechange
 
   name    = "crossfeed.${data.aws_route53_zone.cyber_dhs_gov.name}"
+  records = ["cyber-hygiene.cisa.dhs.gov.", ]
   ttl     = 300
   type    = "CNAME"
   zone_id = aws_route53_zone.cyber_dhs_gov.zone_id
-
-  records = [
-    "cyber-hygiene.cisa.dhs.gov.",
-  ]
 }
 
 resource "aws_route53_record" "crossfeed_prod_docs_CNAME" {

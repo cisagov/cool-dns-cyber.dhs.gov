eedd8d873954532e1d898dafd4c47617b5c51279
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2025-08-06 11:14:37 -0400

    Name the cyber.dhs.gov hosted zone based on the Terraform workspace

--- route53_zone.tf
+++ route53_zone.tf
@@ -1,10 +1,10 @@
 # ------------------------------------------------------------------------------
-# Create the cyber.dhs.gov zone and configure it with our primary reusable delegation
-# set.
+# Create the cyber.dhs.gov zone and configure it with our primary reusable
+# delegation set.
 # ------------------------------------------------------------------------------
 resource "aws_route53_zone" "cyber_dhs_gov" {
   provider = aws.dnsprovisionaccount
 
   delegation_set_id = data.terraform_remote_state.dns.outputs.primary_delegation_set.id
-  name              = "cyber.dhs.gov"
+  name              = terraform.workspace == "production" ? "cyber.dhs.gov" : "${terraform.workspace}.cyber.dhs.gov"
 }

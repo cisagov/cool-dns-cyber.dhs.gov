e8726fd31cd171c2a689a7816a13e9f735a9825b
cduhn17 <craig.duhn@nttdatafed.com>
2026-06-23 14:05:19 -0500

    Update acme-challenge.staging-cd.crossfeed value to issue new LE cert second try.

--- route53_crossfeed_app.tf
+++ route53_crossfeed_app.tf
@@ -452,7 +452,7 @@ resource "aws_route53_record" "crossfeed_staging_cd_TXT" {
 
   name = "_acme-challenge.staging-cd.crossfeed.${aws_route53_zone.cyber_dhs_gov.name}"
   records = [
-    "996gfmlMMt4Zf4-3cwZXlCF2BSjeDjpuZ7ALw6K6FB8",
+    "TiafKmY0uf8yr8CPVPt-6OhQB1ySZMI_oG1ksgvGoYc",
   ]
   ttl     = 3000
   type    = "TXT"
